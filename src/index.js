2c0cc39aeeb1cd415b84bd19722f2a5a3765f6b2
pallavh2020 <pallavhirpara4@gmail.com>
2026-03-19 13:56:32 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config(
+    {
+    path : './.env'
+    }
+)
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /*
+// import express from "express";
+
+// const app = express()
+
+// (async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         app.on('ERROR',(error)=>{
+//             console.log("ERROR",error);
+//             throw error
+//         })    
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`App i running on port ${process.env.PORT}`);
+            
+//         })
+//     } catch (error) {
+//         console.error('ERROR',error)
+//         throw error
+
+//     }
+// })()
+//     */
